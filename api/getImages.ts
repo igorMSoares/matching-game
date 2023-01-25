df3d6fc74e04de771b46332a3b3612a258c9b0db
igorMSoares <igorms.dev@gmail.com>
2023-01-24 21:47:52 -0300

    feat(api): remove videos from the result, return images only

--- api/getImages.ts
+++ api/getImages.ts
@@ -3,17 +3,31 @@ import axios from 'axios';
 
 const { APOD_BASE_URL, APOD_KEY_NAME, APOD_API_KEY } = process.env;
 
+type APODImg = {
+  date: string;
+  explanation: string;
+  hdurl: string;
+  media_type: string;
+  service_version: string;
+  title: string;
+  url: string;
+};
+
 const api = axios.create({
   baseURL: APOD_BASE_URL,
 });
 
 export default async (req: VercelRequest, res: VercelResponse) => {
+  const count = Number(req.query.count ?? 10);
   try {
-    const images = await api.get(
-      `/apod?${APOD_KEY_NAME}=${APOD_API_KEY}&count=${req.query.count ?? 10}`
+    const apiRes = await api.get(
+      `/apod?${APOD_KEY_NAME}=${APOD_API_KEY}&count=${1.5 * count}`
     );
+    const images = apiRes.data
+      .filter((data: APODImg) => data.media_type === 'image')
+      .slice(0, count);
     res.status(200);
-    return res.json(images.data);
+    return res.json(images);
   } catch (error) {
     return res.json({ error: error });
   }
