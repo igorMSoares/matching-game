9ecf27251c11fe9a0158a38c670d594f8b7e8be1
igorMSoares <igorms.dev@gmail.com>
2023-01-28 18:31:59 -0300

    refactor: create ImageDetail component and move all to HomeView

---
+++ src/components/ImageDetail.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+import { useCardDeckStore } from '@/stores/cardDeck';
+
+const cardDeckStore = useCardDeckStore();
+</script>
+
+<template>
+  <div v-if="cardDeckStore.lastMatch">
+    <div class="image-display">
+      <h2>{{ cardDeckStore.lastMatch.image.title }}</h2>
+      <img
+        :src="cardDeckStore.lastMatch.image.url"
+        :alt="cardDeckStore.lastMatch.image.title"
+      />
+      <p>{{ cardDeckStore.lastMatch.image.explanation }}</p>
+    </div>
+  </div>
+</template>
+
+<style scoped></style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,13 +1,44 @@
 <script setup lang="ts">
-import ApodImage from '../components/ApodImage.vue';
+import CardDeck from '../components/CardDeck.vue';
+import ImageDetail from '../components/ImageDetail.vue';
+
+import { ref } from 'vue';
+
+const imagesCount = ref(10);
+
+const err = ref<string | null>(null);
 </script>
 
 <template>
   <main>
+    <div v-if="err">
+      <p>
+        Sorry, a problem has occurred while fetching images from Nasa. Please
+        try again soon.
+      </p>
+    </div>
+
+    <ImageDetail />
+
     <Suspense>
-      <ApodImage />
+      <CardDeck
+        :images-count="imagesCount"
+        @deck-error="(error: string) => (err = error)"
+      />
 
       <template #fallback> Loading... </template>
     </Suspense>
   </main>
 </template>
+
+<style scoped>
+.image-display {
+  height: clamp(20rem, 100%, 25rem);
+  width: clamp(19rem, 80vw, 70%);
+  padding: 2rem 0;
+  font-family: 'Trebuchet MS', 'Lucida Sans Unicode', 'Lucida Grande',
+    'Lucida Sans', Arial, sans-serif;
+  font-size: 1rem;
+  font-weight: bolder;
+}
+</style>
