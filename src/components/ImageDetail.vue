84476796a81dd74ff0ecbedb1231d627df190d5e
igorMSoares <igorms.dev@gmail.com>
2023-02-19 17:49:42 -0300

    feat: modify <ImageDetail> styles

--- src/components/ImageDetail.vue
+++ src/components/ImageDetail.vue
@@ -8,22 +8,35 @@ const { image } = defineProps<{ image: APODImg }>();
   <v-card
     v-if="image"
     variant="elevated"
-    class="image-display mb-6 w-auto bg-blue-grey-lighten-5"
+    class="image-display d-flex flex-column justify-center align-center mb-6 px-6 py-6 bg-blue-grey-lighten-5"
     :elevation="4"
   >
     <v-card-title class="text-center">{{ image.title }}</v-card-title>
-    <v-card-item class="image d-flex justify-center">
-      <v-img :src="image.url" :alt="image.title" width="auto" />
-    </v-card-item>
-    <v-card-text class="text-justify px-9 pt-5">{{
-      image.explanation
-    }}</v-card-text>
+    <v-img :src="image.url" :alt="image.title" width="auto" class="image" />
+    <v-card-text class="explanation text-justify overflow-x-auto pt-0 mt-4">
+      <p>
+        {{ image.explanation }}
+      </p>
+    </v-card-text>
   </v-card>
 </template>
 
 <style scoped>
 .image-display {
-  padding: 2rem 0;
-  margin: 0 auto;
+  height: 80vh;
+}
+
+.image {
+  flex-grow: 0;
+  max-height: 65%;
+}
+
+.explanation {
+  font-size: 1.25rem;
+  font-family: 'Segoe UI', Tahoma, Geneva, Verdana, sans-serif;
+  line-height: 1.25em;
+  flex-grow: 0;
+  max-width: 90ch;
+  min-width: 38ch;
 }
 </style>
