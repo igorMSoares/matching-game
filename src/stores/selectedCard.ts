e7c55aec82bbb8442f00a5d83e479a57ecf7cd0c
igorMSoares <igorms.dev@gmail.com>
2023-03-04 20:45:15 -0300

    feat: if don't match, cards will be toggled back after 500ms

--- src/stores/selectedCard.ts
+++ src/stores/selectedCard.ts
@@ -1,5 +1,5 @@
 import { defineStore } from 'pinia';
-import { ref } from 'vue';
+import { ref, computed } from 'vue';
 import { useCardDeckStore } from './cardDeck';
 
 export const useSelectedCardStore = defineStore('selectedCardStore', () => {
@@ -9,17 +9,21 @@ export const useSelectedCardStore = defineStore('selectedCardStore', () => {
   function add(card: Card) {
     if (!card.isSelected) return;
 
-    const totalSelected = selectedCards.value.length;
-
-    if (totalSelected === 1) {
+    if (totalSelected.value === 1) {
       const cardA = selectedCards.value[0];
       if (match(card, cardA)) cardDeckStore.matchFound(card.id, cardA.id);
-    } else if (totalSelected === 2) {
+      else
+        setTimeout(() => {
+          if (totalSelected.value === 2) clear();
+        }, 500);
+    } else if (totalSelected.value === 2) {
       clear();
     }
 
     selectedCards.value.push(card);
   }
+
+  const totalSelected = computed(() => selectedCards.value.length);
 
   function match(cardA: Card, cardB: Card) {
     return cardA.key === cardB.key;
