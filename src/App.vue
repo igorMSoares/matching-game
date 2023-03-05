9e20e02f2d848d52387964e4ee4f1e74dc8709ea
igorMSoares <igorms.dev@gmail.com>
2023-03-05 19:22:56 -0300

    feat: remove toolbar from App

--- src/App.vue
+++ src/App.vue
@@ -5,14 +5,6 @@ import Loading from './components/Loading.vue';
 
 <template>
   <v-app class="app-min-width">
-    <v-toolbar
-      dark
-      title="Matching Game"
-      density="compact"
-      :elevation="2"
-      class="bg-blue-grey-darken-1"
-    ></v-toolbar>
-
     <v-main>
       <v-container class="main-container">
         <RouterView v-slot="{ Component }">
