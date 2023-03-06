a8221912911fc691845bdb21920767040a7dc44c
igorMSoares <igorms.dev@gmail.com>
2023-03-06 20:18:49 -0300

    refactor: create <OptionsBtn> component

--- src/components/GameOptions.vue
+++ src/components/GameOptions.vue
@@ -1,5 +1,6 @@
 <script setup lang="ts">
 import { toRefs } from 'vue';
+import OptionsBtn from './OptionsBtn.vue';
 
 defineEmits(['newGame', 'restartGame', 'startGame', 'refetchImages']);
 
@@ -8,62 +9,41 @@ const { gameStarted } = toRefs(props);
 </script>
 
 <template>
-  <v-hover v-slot="{ isHovering, props }">
-    <div
-      class="icons-wrapper d-flex justify-center align-center"
-      :class="!gameStarted ? 'flex-column' : ''"
-    >
-      <v-btn
-        v-if="gameStarted"
-        class="ma-auto text-h6 text-brown-darken-4 w-xs-100"
-        color="amber-accent-3"
-        height="3rem"
-        v-bind="props"
-        :elevation="isHovering ? 8 : 5"
-        @click="$emit('newGame')"
-      >
-        <v-icon class="mr-1">mdi-autorenew</v-icon>
-        New Game
-      </v-btn>
+  <div class="icons-wrapper d-flex justify-center align-center">
+    <OptionsBtn
+      v-if="gameStarted"
+      :iconName="'mdi-autorenew'"
+      :btnText="'New Game'"
+      @click="$emit('newGame')"
+    />
 
-      <v-btn
-        v-if="gameStarted"
-        class="ma-auto text-h6 text-brown-darken-4"
-        color="amber-accent-3"
-        height="3rem"
-        v-bind="props"
-        :elevation="isHovering ? 8 : 5"
-        @click="$emit('restartGame')"
-      >
-        <v-icon class="mr-1">mdi-reload</v-icon>
-        Restart Game
-      </v-btn>
+    <OptionsBtn
+      v-if="gameStarted"
+      :iconName="'mdi-reload'"
+      :btnText="'Restart Game'"
+      @click="$emit('restartGame')"
+    />
 
+    <v-hover v-slot="{ isHovering, props }">
       <v-btn
         v-if="!gameStarted"
         title="Generate new cards"
         icon="mdi-restart"
-        class="text-brown-darken-4 restart-icon"
+        :class="`text-brow-darken-4 restart-icon`"
         color="amber-accent-1"
         v-bind="props"
         :elevation="isHovering ? 8 : 5"
         @click="$emit('refetchImages')"
       />
+    </v-hover>
 
-      <v-btn
-        v-if="!gameStarted"
-        class="ma-3 text-h6 text-brown-darken-4"
-        color="amber-accent-3"
-        height="3rem"
-        v-bind="props"
-        :elevation="isHovering ? 8 : 5"
-        @click="$emit('startGame')"
-      >
-        <v-icon class="mr-1">mdi-play-circle</v-icon>
-        Start Game
-      </v-btn>
-    </div>
-  </v-hover>
+    <OptionsBtn
+      v-if="!gameStarted"
+      :iconName="'mdi-play-circle'"
+      :btnText="'Start Game'"
+      @click="$emit('startGame')"
+    />
+  </div>
 </template>
 
 <style scoped>
---
+++ src/components/OptionsBtn.vue
@@ -0,0 +1,33 @@
+<script setup lang="ts">
+defineEmits(['click']);
+
+const { iconName, btnText } = defineProps<{
+  iconName: string;
+  btnText: string;
+}>();
+
+const btnProps = {
+  bgColor: 'amber-accent-3',
+  textColor: 'browwn-darken-4',
+  textSize: 'h6',
+  height: '3rem',
+};
+</script>
+
+<template>
+  <v-hover v-slot="{ isHovering, props }">
+    <v-btn
+      :class="`ma-auto text-${btnProps.textSize} text-${btnProps.textColor}`"
+      :color="`${btnProps.bgColor}`"
+      :height="`${btnProps.height}`"
+      v-bind="props"
+      :elevation="isHovering ? 8 : 5"
+      @click="$emit('click')"
+    >
+      <v-icon class="mr-1">{{ iconName }}</v-icon>
+      {{ btnText }}
+    </v-btn>
+  </v-hover>
+</template>
+
+<style scoped></style>
